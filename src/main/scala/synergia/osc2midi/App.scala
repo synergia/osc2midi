b12837ee4a4425a4a4f3f789da0fa20247511e7d
Tymon Tobolski <i@teamon.eu>
2010-09-03 16:08:02 +0200

    Fixed argument parsing

--- src/main/scala/synergia/osc2midi/App.scala
+++ src/main/scala/synergia/osc2midi/App.scala
@@ -2,7 +2,7 @@ package synergia.osc2midi
 
 object App {
 	def main(args: Array[String]) {
-		val port = if(args.length == 1) args(1).toInt else 4444
+		val port = if(args.length == 1) args(0).toInt else 4444
 		val listener = new Listener(port)
 		listener.connect
 	}
